409b0b24c2361121df5968bc6586362af4f9dd57
Carlos Araújo <car_soad@hotmail.com>
2018-08-30 14:43:32 -0300

    primeira aula

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  photos = [
+    {
+      url: 'https://img.pokemondb.net/artwork/large/arcanine.jpg',
+      description: 'Arcanine'
+    },
+    {
+      url: 'https://img.pokemondb.net/artwork/large/feraligatr.jpg',
+      description: 'Feraligatr'
+    }
+  ];
 }
